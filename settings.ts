e54f62feb68ec6479ea366db23f926376dab5dc2
cartel <119452317+cartelfx@users.noreply.github.com>
2024-04-14 07:10:41 +0400

    Create settings.ts

---
+++ settings.ts
@@ -0,0 +1,13 @@
+export const tokens: string[] = [
+
+    "MTIyNzYxMDQxMTI4MTY4MjQ5NA.GIXuBv.xd",
+
+    "MTIyNzYyMTM2MDY3MTM5MTc1NQ.G1WY62.xd"
+
+];
+
+
+
+export const channel: string = "ID";
+
+export const guild: string = "ID";
